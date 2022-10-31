7f2bef16bc8831119af3b8f12e6d504ca7910d67
Thiago Jabur Bittar <31863285+thiagojabur@users.noreply.github.com>
2022-10-31 16:19:12 -0300

    Update Principal.java

--- src/Principal.java
+++ src/Principal.java
@@ -5,13 +5,22 @@ public class Principal {
 		
 
 		Node b = new Node("B", new Node("D", null, null),new Node("E", null, null));
-		Node raiz = new Node("A", b, null);
+		Node c = new Node("C", new Node("F", null, null),new Node("G", null, null));
+		Node raiz = new Node("A", b, c);
+		
+		
 		
 		BinaryTree arvoreBinariaTeste = new BinaryTree(raiz);
 	
 		
-		
+		System.out.print("Pré ordem: ");
 		arvoreBinariaTeste.preOrderPrint(raiz);
+		System.out.println("");
+		System.out.print("Em ordem: ");
+		arvoreBinariaTeste.inOrderPrint(raiz);
+		System.out.println("");
+		System.out.print("Pós ordem: ");
+		arvoreBinariaTeste.postOrderPrint(raiz);
 	}
 
 }
