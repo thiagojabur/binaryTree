5612493deed6e77559eeed8f325745ce15477600
Thiago Jabur Bittar <31863285+thiagojabur@users.noreply.github.com>
2022-11-01 09:42:52 -0300

    Update Principal.java

--- src/Principal.java
+++ src/Principal.java
@@ -3,16 +3,18 @@ public class Principal {
 
 	public static void main(String[] args) {
 		
-
-		Node b = new Node("B", new Node("D", null, null),new Node("E", null, null));
+		Node d = new Node("D", null, null);
+		Node e = new Node("E", null, null);
+		Node b = new Node("B", d, e);
 		Node c = new Node("C", new Node("F", null, null),new Node("G", null, null));
 		Node raiz = new Node("A", b, c);
 		
-		
-		
+		System.out.println("Raiz é externa: " +  raiz.isExternal());
+		System.out.println("D é externa: " + d.isExternal());
 		BinaryTree arvoreBinariaTeste = new BinaryTree(raiz);
-	
 		
+		System.out.println("Grau da Raiz: " +  raiz.length());
+		System.out.println("Grau do nó E: " +  e.length());
 		System.out.print("Pré ordem: ");
 		arvoreBinariaTeste.preOrderPrint(raiz);
 		System.out.println("");
