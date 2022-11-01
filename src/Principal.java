5a721ade463f83516749ed817fc635f119bcd840
Thiago Jabur Bittar <31863285+thiagojabur@users.noreply.github.com>
2022-11-01 17:13:47 -0300

    Update Principal.java

--- src/Principal.java
+++ src/Principal.java
@@ -1,19 +1,20 @@
 
 public class Principal {
-
 	public static void main(String[] args) {
 		
-		Node d = new Node("D", null, null);
+		Node d = new Node("D", null, new Node("T", null,null));
 		Node e = new Node("E", null, null);
+		Node h = new Node("H", null,null);
 		Node b = new Node("B", d, e);
-		Node c = new Node("C", new Node("F", null, null),new Node("G", null, null));
+		Node c = new Node("C", new Node("F", null, null),new Node("G",h , null));
 		Node raiz = new Node("A", b, c);
+		
 		
 		System.out.println("Raiz é externa: " +  raiz.isExternal());
 		System.out.println("D é externa: " + d.isExternal());
 		BinaryTree arvoreBinariaTeste = new BinaryTree(raiz);
 		
-		System.out.println("Grau da Raiz: " +  raiz.length());
+		System.out.println("Grau da Raiz: " +  arvoreBinariaTeste.getRoot().length());
 		System.out.println("Grau do nó E: " +  e.length());
 		System.out.print("Pré ordem: ");
 		arvoreBinariaTeste.preOrderPrint(raiz);
@@ -23,6 +24,16 @@ public class Principal {
 		System.out.println("");
 		System.out.print("Pós ordem: ");
 		arvoreBinariaTeste.postOrderPrint(raiz);
+		System.out.println("");
+		System.out.println("É estritamente binária: ");
+		System.out.print(arvoreBinariaTeste.isStrictBinaryTree(raiz));
+		System.out.println("");
+		System.out.println("Quantidade de nós: ");
+		System.out.println(arvoreBinariaTeste.findNodeAmount(raiz));
+
+		System.out.println("Quantidade máxima de níveis (profundidade): ");
+		System.out.println(arvoreBinariaTeste.findDepth(raiz));
+	
 	}
 
 }
